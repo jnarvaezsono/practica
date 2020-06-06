c345c9582689640f4dae716cd4b3551069603f25
Jose Narvaez <jose.narvaez@sonovista.co>
2020-06-05 20:10:24 -0500

    merge

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const Sentry = require('@sentry/node');
 
 const api = require('./api');
 const config = require('./config');
 
+Sentry.init({ dsn: 'https://fbcad5b9c86a47f6aa4ae1aa7bd2bfa5@o403586.ingest.sentry.io/5266422' });
 const app = express();
 app.use(express.json());
 app.use('/api', api);
